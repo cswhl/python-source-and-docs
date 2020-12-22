a9893cd10c66684eb8cc496313469e9ced34464f
cswhl <cswhltime@126.com>
2020-12-22 07:31:41 +0000

    update

--- socketserver/server_threads.py
+++ socketserver/server_threads.py
@@ -12,12 +12,13 @@ class MyRequestHandler(SRH):
 
     def handle(self):
         # 请求处理
-        print(threading.current_thread())
+        # print(threading.current_thread())
         print(f'...connected from:{self.client_address}')
-        print(self.request)
+        # print(self.request)
         self.wfile.write(f'[{ctime()}] {self.rfile.readline()}'.encode('utf8') )
+
 
 with ThreadingTCPServer(ADDR, MyRequestHandler) as tcpServ:
     print('waiting for connection')
-    print(threading.current_thread())
+    # print(threading.current_thread())
     tcpServ.serve_forever() # 运行服务器，直到shutdown()
