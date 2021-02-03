3b41bbf4d4bb29ea205ad9b9f9731888240ef3ae
cswhl <cswhltime@126.com>
2021-02-03 13:48:00 +0000

    reconstruct

--- 99-SideProject/WSGI/web_server.py
+++ 99-SideProject/WSGI/web_server.py
@@ -6,7 +6,6 @@ import re
 import sys
 import mini_frame
 
-ADDR = '0.0.0.0', int(sys.argv[1])
 client_addr = []
 client_socket = []
 
@@ -21,64 +20,6 @@ class HttpConst(object):
     GET = 'GET '
     POST = 'POST '
     HOST = 'Host: '
-
-
-class HttpHandler(object):
-    def __init__(self, request_data):
-        self.request_data = request_data
-
-    def _set_path_info(self):
-        # 1. get file_name
-        request_data_lines = self.request_data.splitlines()
-        print(request_data_lines)
-        file_name = ""
-        ret = re.match(r"[^/]+(/[^ ]*)", request_data_lines[0])
-        if ret:
-            file_name = '/index.html' if ret.group(
-                1) == '/' else ret.group(1)
-
-        self.file_name = file_name
-
-    def _set_static_source(self):
-        self.response_headers = ''
-        try:
-            with open('./html' + self.file_name, 'rb') as f:
-                self.response_body = f.read()
-        except Exception:
-            self.response_line = "HTTP/1.1 404 NOT FOUND"
-            self.response_body = 'file not found'.encode('utf8')
-        else:
-            self.response_line = "HTTP/1.1 200 OK"
-
-    def _set_dynamic_source(self):
-        self.response_headers = ''
-        env = dict()
-        env['path_info'] = self.file_name
-        self.response_body = mini_frame.application(
-            env, self.set_response_header).encode('utf8')
-        self.response_line = f'HTTP/1.1 {self.status}'
-        for header in self.headers:
-            self.response_headers += f'{header[0]}: {header[1]}' + HttpConst.CRLF
-
-    def _set_response(self):
-        self.response = self.response_line + HttpConst.CRLF
-        self.response += self.response_headers + HttpConst.CRLF
-        self.response = self.response.encode('utf8') + self.response_body
-
-    def get_response(self):
-        self._set_path_info()
-
-        if not self.file_name.endswith(".py"):
-            self._set_static_source()
-        else:
-            self._set_dynamic_source()
-        self._set_response()
-
-        return self.response
-
-    def set_response_header(self, status, headers):
-        self.status = status
-        self.headers = [('server', 'web v1.0')] + headers
 
 
 class ThreadedTCPRequestHandler(BaseRequestHandler):
@@ -102,7 +43,7 @@ class ThreadedTCPRequestHandler(BaseRequestHandler):
                 print(self.ip + ":" + str(self.port) + "接收超时！即将断开连接！")
                 break       # 记得跳出while循环
 
-            if request_data:    # 判断是否接收到数据
+            if request_data:
                 http_handler = HttpHandler(request_data)
                 response = http_handler.get_response()
                 self.request.send(response)
@@ -114,7 +55,87 @@ class ThreadedTCPRequestHandler(BaseRequestHandler):
         client_socket.remove(self.request)
 
 
+class HttpHandler(object):
+    def __init__(self, request_data):
+        self.request_data = request_data
+        self.file_path = self._get_path_of_file()
+
+    def _get_path_of_file(self):
+        request_data_lines = self.request_data.splitlines()
+        ret = re.match(r"[^/]+(/[^ ]*)", request_data_lines[0])
+        file_path = ""
+        if ret:
+            file_path = '/index.html' if ret.group(
+                1) == '/' else ret.group(1)
+
+        return file_path
+
+    def get_response(self):
+        if not self.file_path.endswith(".py"):
+            self._set_static_page_info()
+        else:
+            self._set_dynamic_page_info()
+        self._combinate_response_page_info()
+
+        return self.response
+
+    def _combinate_response_page_info(self):
+        self.response = self.response_line + HttpConst.CRLF
+        self.response += self.response_headers + HttpConst.CRLF
+        self.response = self.response.encode('utf8') + self.response_body
+
+    def _set_static_page_info(self):
+        self.response_headers = ''
+        try:
+            with open('./html' + self.file_path, 'rb') as f:
+                self.response_body = f.read()
+        except Exception:
+            self.response_line = "HTTP/1.1 404 NOT FOUND"
+            self.response_body = 'file not found'.encode('utf8')
+        else:
+            self.response_line = "HTTP/1.1 200 OK"
+
+    def _set_dynamic_page_info(self):
+        env = dict()
+        env['path_info'] = self.file_path
+        self.response_body = mini_frame.application( env, self.set_response_header).encode('utf8') # noqa
+        self.response_line = f'HTTP/1.1 {self.status}'
+        self.response_headers = ''
+        for header in self.headers:
+            self.response_headers += f'{header[0]}: {header[1]}' + HttpConst.CRLF # noqa
+
+    def set_response_header(self, status, headers):
+        self.status = status
+        self.headers = [('server', 'web v1.0')] + headers
+
+
+def get_():
+    if len(sys.argv) == 3:
+        port = sys.argv[1]
+        frame_app_name = sys.argv[2]
+        print(port, frame_app_name)
+    else:
+        print("请按以下方式运行:")
+        print("python3 xxx.py 9999 mini_frame:application")
+        return
+
+    ret = re.match(r'([^:]+):(.*)', frame_app_name)
+    if ret:
+        frame_name = ret.group(1)
+        app_name = ret.group(2)
+        print(frame_name, app_name)
+    else:
+        print("请按以下方式运行:")
+        print("python3 xxx.py 9999 mini_frame:application")
+        return
+
+    frame = __import__(frame_name)
+    app = getattr(frame, app_name)
+
+
 if __name__ == '__main__':
+    ADDR = '0.0.0.0', int(sys.argv[1])
+    get_()
     ThreadingTCPServer.allow_reuse_address = True  # 允许地址复用
     with ThreadingTCPServer(ADDR, ThreadedTCPRequestHandler) as wsgi_server:
         print('waiting for connection')
